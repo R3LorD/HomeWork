222c35d007fc5203f58eae4790bcb38023bc8976
Alex <gyperluck@gmail.com>
2020-01-07 22:46:38 +0300

    Start

--- home/src/app/app.module.ts
+++ home/src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddListComponent } from './add-list/add-list.component';
+import { ListsComponent } from './lists/lists.component';
+import { EditListComponent } from './edit-list/edit-list.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddListComponent,
+    ListsComponent,
+    EditListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
