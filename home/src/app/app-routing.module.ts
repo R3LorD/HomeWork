cc573cd359df305ec22e53a42f19f1aa4366eaac
Alex <gyperluck@gmail.com>
2020-01-07 20:13:58 +0300

    New homework

--- home/src/app/app-routing.module.ts
+++ home/src/app/app-routing.module.ts
@@ -1,13 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { LifeComponent } from './life/life.component';
-import { SecondComponent } from './second/second.component';
 
 
-const routes: Routes = [
-  {path: '', component: LifeComponent},
-  {path: 'second', component: SecondComponent}
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
